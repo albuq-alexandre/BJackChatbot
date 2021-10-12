4b556075f441274778612b9f4c1029a068b15000
Alexandre Albuquerque <albuq.alexandre@gmail.com>
2021-10-12 16:08:54 -0300

    requeriments fix for heroku

--- config_telegram.py
+++ config_telegram.py
@@ -12,9 +12,9 @@ import logging
 
 #Variaveis de ambiente de configuracao
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
-PORT = int(os.environ.get('PORT', '8443'))
+PORT = int(os.environ.get('$PORT', '8443'))
 WEBHOOK_URL = os.environ.get('TELEGRAM_WEBHOOK')
-PORT = 8443
+# PORT = 8443
 
 #configura logging e config
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
