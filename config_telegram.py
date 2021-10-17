19446f9b56c305df1e68672720944bc5e1b455de
Alexandre Albuquerque <albuq.alexandre@gmail.com>
2021-10-17 05:24:26 -0300

    comment ngrok for heroku build

--- config_telegram.py
+++ config_telegram.py
@@ -9,16 +9,16 @@ import assistant
 import voice
 import os
 import logging
-from pyngrok import ngrok
+# from pyngrok import ngrok
 
 #Variaveis de ambiente de configuracao
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 PORT = int(os.environ.get('$PORT', '8443'))
 WEBHOOK_URL = os.environ.get('TELEGRAM_WEBHOOK')
 
-if not WEBHOOK_URL:
-    http_tunnel = ngrok.connect(PORT, bind_tls=True)
-    WEBHOOK_URL = http_tunnel.public_url
+# if not WEBHOOK_URL:
+#     http_tunnel = ngrok.connect(PORT, bind_tls=True)
+#     WEBHOOK_URL = http_tunnel.public_url
 
 
 
